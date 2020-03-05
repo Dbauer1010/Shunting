1a9f1f3bb688c4d6921e06a3ab0d74f03c34b170
Dbauer1010 <56281163+Dbauer1010@users.noreply.github.com>
2020-03-04 20:07:45 -0600

    Driver.cpp

---
+++ Driver.cpp
@@ -0,0 +1,16 @@
+#include<string.h>
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  char* s[] = "123+13-2";
+  char* delim[] = "+-*/";
+  char* tempstring;
+  char* main;
+  do
+  {
+    tempstring = strtok(s,delim);
+    main[]->add->tempString;
+  }
+}
