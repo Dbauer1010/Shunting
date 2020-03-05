9af4c7c0b08f0bde8740c538b64ba89b2fee0248
Dbauer1010 <56281163+Dbauer1010@users.noreply.github.com>
2020-03-04 20:20:30 -0600

    Update Driver.cpp

--- Driver.cpp
+++ Driver.cpp
@@ -7,10 +7,25 @@ int main()
   char* s[] = "123+13-2";
   char* delim[] = "+-*/";
   char* tempstring;
-  char* main;
+  char* main[];
+  char* tempdelim;
   do
   {
     tempstring = strtok(s,delim);
-    main[]->add->tempString;
+    main[0] = tempstring;
+    tempdelim = strtok(delim, s);
+    main[1] = tempdelim;
+    delim[]->shift();
+    tempstring = strtok(s,delim);
+    main[2] = tempstring;
+    tempdelim = strtok(delim, s)
+    main[3] = tempdelim;
+    delim[]->shift();
+    tempstring = strtok(s,delim);
+    main[4] = tempstring;
+    
+    
+    
+    
   }
 }
