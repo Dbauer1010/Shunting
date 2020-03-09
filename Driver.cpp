61817f79ff2195ad5d691aeed1718b864617f56e
Dbauer1010 <56281163+Dbauer1010@users.noreply.github.com>
2020-03-09 11:05:36 -0500

    Update Driver.cpp

--- Driver.cpp
+++ Driver.cpp
@@ -1,31 +1,17 @@
-#include<string.h>
 #include<iostream>
+#include "StringFactory.hpp"
 using namespace std;
 
 int main()
 {
-  char* s[] = "123+13-2";
-  char* delim[] = "+-*/";
-  char* tempstring;
-  char* main[];
-  char* tempdelim;
-  do
-  {
-    tempstring = strtok(s,delim);
-    main[0] = tempstring;
-    tempdelim = strtok(delim, s);
-    main[1] = tempdelim;
-    delim[]->shift();
-    tempstring = strtok(s,delim);
-    main[2] = tempstring;
-    tempdelim = strtok(delim, s)
-    main[3] = tempdelim;
-    delim[]->shift();
-    tempstring = strtok(s,delim);
-    main[4] = tempstring;
-    
-    
-    
-    
-  }
+    string s = "123+42-8*2";
+    LinkedList* ll = new LinkedList();
+    ll->addEnd("hello");
+    ll->addEnd("world");
+    ll->addEnd("lol");
+    ll->addEnd("woot");
+    ll->display();
+
+    StringFactory::split(s,"+-*/");
+
 }
