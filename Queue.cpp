995da12bdeacb112870f7fe9905d938e21069517
Dbauer1010 <56281163+Dbauer1010@users.noreply.github.com>
2020-04-01 20:26:09 -0500

    Create Queue.cpp

---
+++ Queue.cpp
@@ -0,0 +1,15 @@
+#include Queue.hpp;
+
+void Enqueue(int val)
+{
+  if(front == -1) front = 0;
+  {   
+      rear++;
+      myqueue[rear] = value;
+      cout << value << " ";
+   }
+   else
+   {
+      
+   }
+}
