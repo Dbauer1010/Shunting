91a3cc9e87108b35bcc11904239c52b5724157e2
Dbauer1010 <56281163+Dbauer1010@users.noreply.github.com>
2020-04-01 20:31:13 -0500

    Update Queue.hpp

--- Queue.hpp
+++ Queue.hpp
@@ -5,5 +5,12 @@ using namespace std;
 
 class Queue
 {
+    public:
+      void Enqueue(int val);
+      void Dequeue(int val);
+      int front = -1;
+      int rear = -1;
   
+    private:
+      int queue[MAX_SIZE], front, rear;
 }
