ecd4022f5334f36e2446e761ead20b45f755a567
Dbauer1010 <56281163+Dbauer1010@users.noreply.github.com>
2020-04-01 20:18:40 -0500

    Update StringFactory.cpp

--- StringFactory.cpp
+++ StringFactory.cpp
@@ -1,24 +1,11 @@
 #include "StringFactory.hpp"
-#include <iostream>
 
-using namespace std;
-
-string* StringFactory::split(string s, string delims)
+Queue* StringFactory::split(string s, string delims)
 {
-    char* s[] = s;
-    char* delim[] = delims;
-    char* tempstring;
-    char* main[];
-    char* tempdelim;
-    do
-    {
-      for(int i = 0; i < s[].length(); ++i)
-	{
-    	  tempstring = strtok(s,delim);
-    	  main[i] = tempstring;
-     	  tempdelim = strtok(delim, s);
-     	  main[i+1] = tempdelim;
-     	  delim[]->shift();
-     	}
-    }
+    cout << "write your code here \n";
+    /*
+    update split to return a Queue of strings that will represent our
+    input queue for shunting yard.  This will require you to implement
+    a Queue data structure that supports enqueue and dequeue
+    */
 }
