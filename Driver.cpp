6227185b56ecd325e5a7438c76e86bcf6e8a514a
Dbauer1010 <56281163+Dbauer1010@users.noreply.github.com>
2020-04-06 17:13:07 -0500

    Update Driver.cpp

--- Driver.cpp
+++ Driver.cpp
@@ -1,17 +1,24 @@
 #include<iostream>
 #include "StringFactory.hpp"
+#include "Queue.hpp"
+#include "Stack.hpp"
+
 using namespace std;
 
 int main()
 {
     string s = "123+42-8*2";
-    LinkedList* ll = new LinkedList();
-    ll->addEnd("hello");
-    ll->addEnd("world");
-    ll->addEnd("lol");
-    ll->addEnd("woot");
-    ll->display();
+    string ops = "+-*/";
+    Queue* inputQ = StringFactory::split(s,ops); 
+    inputQ->display();
 
-    StringFactory::split(s,"+-*/");
+    Stack* myStack = new Stack();
+    for(int i = 0; i < 10; i++)
+    {
+        myStack->push((string)"" + to_string(i));
+    }
 
+    myStack->display();
 }
+
+ 
